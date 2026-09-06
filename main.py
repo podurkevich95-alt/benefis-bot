5f6970989e61c959c73112cc7be051c8ab56031f
podurkevich95-alt <podurkevich95@mail.ru>
2026-09-07 02:12:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ async def chat_handler(message: types.Message):
         await message.answer("Извините, произошла ошибка при обработке запроса. Попробуйте еще раз позже.")
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
